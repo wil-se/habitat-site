f9dadf32c2d822ed6197f975fd950c1024989f30
William <sebastiani.1753672@studenti.uniroma1.it>
2022-07-18 14:14:37 +0200

    alt text

--- src/App.js
+++ src/App.js
@@ -85,7 +85,7 @@ export default function App() {
   return (
     <>
     <div style={{textAlign: "center"}}>
-    <img src={Logo} style={{width:700, marginTop: 20}}/>
+    <img alt="Logo" src={Logo} style={{width:700, marginTop: 20}}/>
     <div style={{position: "fixed", top: 0, left: 0, width: "100%", height: "100%", zIndex: -1}}>
     <Canvas
       concurrent
