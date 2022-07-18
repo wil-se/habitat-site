3acf08c35d8f500f5306c3adfe7dd4d24b773930
William <sebastiani.1753672@studenti.uniroma1.it>
2022-07-18 14:58:43 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -85,7 +85,9 @@ export default function App() {
   return (
     <>
     <div style={{textAlign: "center"}}>
-    <img alt="Logo" src={Logo} style={{width:700, marginTop: 20}}/>
+      <div style={{height: "15vw"}}>
+    <img alt="Logo" src={Logo} style={{width:"40%",}}/>
+    </div>
     <div style={{position: "fixed", top: 0, left: 0, width: "100%", height: "100%", zIndex: -1}}>
     <Canvas
       concurrent
