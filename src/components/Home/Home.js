35a00e4cd5d0074138ca60dcd7ad838434834dae
William <sebastiani.1753672@studenti.uniroma1.it>
2022-07-27 20:59:32 +0200

    fix

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -36,29 +36,20 @@ function Home() {
         <Slider/>
       </Container>
         
+        <Container style={{maxWidth: "99%",}}>
       <Card className="my-2 mx-2 py-4">
         <Row className="align-items-center">
-          <Col className="text-center align-text-middle my-2" xs={12} md={6} style={{height: "150px"}}>
+          <Col className="text-center align-text-middle my-2 py-2" xs={12} md={6}>
             <img style={{width: window.innerHeight > 600 ? "60%" : "90%",}} alt="opensea" src="https://storage.googleapis.com/opensea-static/Logomark/OpenSea-Full-Logo%20(dark).svg"/>
           </Col>
-          <Col className="text-center my-2" xs={12} md={6}>
-            <Button className="py-2 px-4"><h3>See the collection</h3></Button>
+          <Col className="text-center my-2 py-2 pt-xs-4" xs={12} md={6}>
+          <img style={{width: window.innerHeight > 600 ? "60%" : "90%",}} alt="spatial" src="https://s.yimg.com/uu/api/res/1.2/FhQZR.hFVYnZ8ZlYQDOjWg--~B/aD0xMjk7dz00ODA7YXBwaWQ9eXRhY2h5b24-/https://media.zenfs.com/en/business-wire.com/bfd0e2193be5ac3fa427ac198b25fc84"/>
           </Col>
         </Row>
       </Card>
+      </Container>
 
-      <Card className="mt-2 mx-2 py-4">
-        <Row className="align-items-center">
-          <Col className="text-center my-2" xs={12} md={6} style={{height: "150px"}}>
-            <img style={{width: window.innerHeight > 600 ? "60%" : "90%",}} alt="spatial" src="https://s.yimg.com/uu/api/res/1.2/FhQZR.hFVYnZ8ZlYQDOjWg--~B/aD0xMjk7dz00ODA7YXBwaWQ9eXRhY2h5b24-/https://media.zenfs.com/en/business-wire.com/bfd0e2193be5ac3fa427ac198b25fc84"/>
-          </Col>
-          <Col className="text-center my-2" xs={12} md={6}>
-            <Button className="py-2 px-4"><h3>Enter the metaverse</h3></Button>
-          </Col>
-        </Row>
-      </Card>
-
-      <Container style={{maxWidth: "100%",}}>
+      <Container style={{maxWidth: "99%",}}>
         <Row className="justify-content-center">
           <Col xs={12} md={6} className="px-1">
             <Container className="bullet px-1 py-2" style={{borderRadius: "0.375rem"}}>
