f4dc7bcbd8d039935c3d2706be7c5f8453971d26
William <sebastiani.1753672@studenti.uniroma1.it>
2022-07-27 20:43:21 +0200

    fix

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,24 +1,21 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Card } from "react-bootstrap";
 import Logo from "../../Assets/logo.png"
 import MainModel from "./MainModel"
 import Slider from "../Slider/Slider";
+import Button from "react-bootstrap/Button";
 
 function Home() {
   return (
     <>
-      
-      
-      
       <Container>
           <Row className="pt-5 mb-5">
             <img alt="logo" src={Logo}/>
           </Row>
       </Container>
       
-      <Container className="py-5" style={{
+      <Row className="py-5 mx-2" style={{
         backgroundColor: "#FBFBFD",
-        maxWidth: "100%"
         }}>
         <Row className="justify-content-center">
           <Col xs={12} md={8}>
@@ -26,8 +23,7 @@ function Home() {
             <h3 className="text-end">-Dodinsky</h3>
           </Col>
         </Row> 
-      </Container>
-
+      </Row>
 
       <Container style={{
         height: window.innerWidth > 600 ? 800 : 360,
@@ -36,10 +32,65 @@ function Home() {
        <MainModel/>
       </Container>
       
-      <Container style={{
-        maxWidth: "100%",
-      }}>
+      <Container style={{maxWidth: "100%",}}>
         <Slider/>
+      </Container>
+        
+      <Card className="my-2 mx-2 py-4">
+        <Row className="align-items-center">
+          <Col className="text-center align-text-middle my-2" xs={12} md={6} style={{height: "150px"}}>
+            <img style={{width: window.innerHeight > 600 ? "60%" : "90%",}} src="https://storage.googleapis.com/opensea-static/Logomark/OpenSea-Full-Logo%20(dark).svg"/>
+          </Col>
+          <Col className="text-center my-2" xs={12} md={6}>
+            <Button className="py-2 px-4"><h3>See the collection</h3></Button>
+          </Col>
+        </Row>
+      </Card>
+
+      <Card className="mt-2 mx-2 py-4">
+        <Row className="align-items-center">
+          <Col className="text-center my-2" xs={12} md={6} style={{height: "150px"}}>
+            <img style={{width: window.innerHeight > 600 ? "60%" : "90%",}} src="https://s.yimg.com/uu/api/res/1.2/FhQZR.hFVYnZ8ZlYQDOjWg--~B/aD0xMjk7dz00ODA7YXBwaWQ9eXRhY2h5b24-/https://media.zenfs.com/en/business-wire.com/bfd0e2193be5ac3fa427ac198b25fc84"/>
+          </Col>
+          <Col className="text-center my-2" xs={12} md={6}>
+            <Button className="py-2 px-4"><h3>Enter the metaverse</h3></Button>
+          </Col>
+        </Row>
+      </Card>
+
+      <Container style={{maxWidth: "100%",}}>
+        <Row className="justify-content-center">
+          <Col xs={12} md={6} className="px-1">
+            <Container className="bullet px-1 py-2" style={{borderRadius: "0.375rem"}}>
+              <Row><h2 className="text-center mt-2">Furnish your metaverse</h2></Row>
+            </Container>
+          </Col>
+          <Col xs={12} md={6} className="px-1">
+            <Container className="bullet px-1 py-2" style={{borderRadius: "0.375rem"}}>
+              <Row><h2 className="text-center mt-2">MetavExpo</h2></Row>
+            </Container>
+          </Col>
+          <Col xs={12} md={6} className="px-1">
+            <Container className="bullet px-1 py-2" style={{borderRadius: "0.375rem"}}>
+              <Row><h2 className="text-center mt-2">Connect with design experts</h2></Row>
+            </Container>
+          </Col>
+          <Col xs={12} md={6} className="px-1">
+            <Container className="bullet px-1 py-2" style={{borderRadius: "0.375rem"}}>
+              <Row><h2 className="text-center mt-2">Digital/Phisycal Twin</h2></Row>
+            </Container>
+          </Col>
+          <Col xs={12} md={6} className="px-1">
+            <Container className="bullet px-1 py-2" style={{borderRadius: "0.375rem"}}>
+              <Row><h2 className="text-center mt-2">Get Rewards</h2></Row>
+            </Container>
+          </Col>
+          <Col xs={12} md={6} className="px-1" style={{borderRadius: "0.375rem"}}>
+            <Container className="bullet px-1 py-2">
+              <Row><h2 className="text-center mt-2">Charity DAO</h2></Row>
+            </Container>
+          </Col>
+        </Row>
       </Container>
     </>
   );
